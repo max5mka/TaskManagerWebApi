3dd9e20125aa3e7add42381821181ec82e2b6719
max5mka <max5mka01@gmail.com>
2026-09-25 18:43:04 +0500

    refactor: change main project path

--- Api.IntegrationTests/ApplicationFactory.cs
+++ Api.IntegrationTests/ApplicationFactory.cs
@@ -5,8 +5,9 @@ namespace Api.IntegrationTests
 {
     public class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
-        private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
-            .WithImage();
+        private readonly MsSqlContainer _dbContainer = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-latest")
+            .WithPassword("Password123!")
+            .Build();
         public Task InitializeAsync()
         {
             throw new NotImplementedException();
