a74fceaad396d5eede3ba2ae98f62200ee2d5f38
max5mka <max5mka01@gmail.com>
2026-09-25 18:17:07 +0500

    refactor: move main project to separate folder

---
+++ Api.IntegrationTests/ApplicationFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.MsSql;
+
+namespace Api.IntegrationTests
+{
+    public class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
+            .WithImage();
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
